a19d6a03987611378244f3f230949413fc84596d
MUHAMMED IRFAN <111655208+0MUHAMMEDIRFAN@users.noreply.github.com>
2024-10-08 06:18:28 +0000

    modified personal details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "MUHAMMED IRFAN",
+  title: "Software Engineer & Developer",
+  email: "irfan@whitejson.com",
+  gitHub: "0muhammedirfan",
+  instagram: "0muhammedirfan",
+  linkedIn: "0muhammedirfan",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "0muhammedirfan",
+  youTube: "0muhammedirfan",
 };
 
 const primaryColor = "#4E567E";
